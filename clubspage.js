55a69181930a4d176843a0ee600bd292d5bd035b
orchid-mantis3 <116598052+orchid-mantis3@users.noreply.github.com>
2024-12-05 00:06:21 -0800

    CSS and headers

--- clubspage.js
+++ clubspage.js
@@ -1,19 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    function setupSlider(sliderId, valueId) {
-        var slider = document.getElementById(sliderId);
-        var output = document.getElementById(valueId);
-        output.innerHTML = slider.value; 
-
-        slider.oninput = function() {
-            output.innerHTML = this.value;
-        }
-    }
-
-    setupSlider("slider1", "value1");
-    setupSlider("slider2", "value2");
-    setupSlider("slider3", "value3");
-
-});
 // When the user scrolls down, adjust the navbar's transparency
 window.onscroll = function() {
     scrollFunction();
